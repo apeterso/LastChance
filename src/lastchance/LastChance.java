44523dfc816afc9232ebcb32c9e86147498f7fbf
apeterso <apeterso@colby.edu>
2014-04-22 00:53:44 -0400

    small fix in lastchance.java

--- src/lastchance/LastChance.java
+++ src/lastchance/LastChance.java
@@ -24,7 +24,95 @@ public class LastChance {
                 + "matches of others. Enter \"exit\" to quit the program.");
         System.out.println("------------------------------------------------------------------------");
         String enter = scanner.nextLine();
-        String contents = new Scanner(new File(args[0])).useDelimiter("\\Z").next();
+        
+        if(enter.equals("sendForm" )){
+            System.out.println("Emailing the entire class of 2014, are you sure? y/n");
+            if(scanner.nextLine().equals("y")){
+                System.out.println("Please enter the name of a file containing email addresses.");
+                String fileName = scanner.nextLine();
+                String emailList = new Scanner(new File(fileName)).useDelimiter("\\Z").next();
+                String[] emails = emailList.split("\n");
+                SendEmail.sendForm(emails);
+            }
+        }
+        else{
+            System.out.println("Please enter the name of a .csv file containing the response data.");
+            String contents = new Scanner(new File(scanner.nextLine())).useDelimiter("\\Z").next();
+            populate(contents);
+            generateMatches();
+            if(enter.equals("sendMatches")){
+                System.out.println("Emailing the entire class of 2014, are you sure? y/n");
+                if(scanner.nextLine().equals("y")){
+                    SendEmail.sendMatches(everyone);
+                }
+            }
+            else{
+                if(enter.equals("exit")){
+                    System.out.println("Goodbye");
+                } 
+                else {
+                    System.out.println("Specify \"matches\" to see someone's matches, \"choices\" to see someone's"
+                            + " choices, or \"me\" to see who listed your name.");
+                    String mode = scanner.nextLine();
+                    if(mode.equals("exit")){
+                        mode = "exit";
+                    }
+                    else{
+                        while(!mode.equals("matches") && !mode.equals("choices") && !mode.equals("me")){
+                            System.out.println();
+                            System.out.println("You must specify \"matches\", \"choices\", or \"me\"");
+                            mode = scanner.nextLine();
+                        }
+                    }
+                    String s = mode;
+                    while(!s.equals("exit")){
+                        try{
+                            if(s.equals("matches") || s.equals("choices") || s.equals("me")){
+                                mode = s;
+                                System.out.println("Name?");
+                                s = scanner.nextLine();
+                            }
+                            if(mode.equals("matches")){
+                                everyone.get(s).printMatches();
+                                System.out.println();
+                            } else if(mode.equals("choices")){
+                                everyone.get(s).printChoices();
+                                System.out.println();
+                            } if(mode.equals("me")){
+                                for(Person p : everyone.values()){
+                                    if(p.getChoices().contains(s)){
+                                        System.out.println(p.getName());
+                                    }
+                                }
+                                System.out.println();
+                            }
+                            s = scanner.nextLine();
+                        }catch (NullPointerException e){
+                            System.out.println("Name not in system.");
+                            s = scanner.nextLine();
+                        }
+                    }
+                    System.out.println("Goodbye");
+                }
+            }
+        }
+    }
+    
+    private static void generateMatches(){
+        for(Person person : everyone.values()){
+            ArrayList<String> choices = person.getChoices();
+            for(String s : choices){
+                if(everyone.containsKey(s)){
+                    Person possibleMatch = everyone.get(s);
+                        if(possibleMatch.getChoices().contains(person.getName())){
+                            person.addMatch(s);
+                        }
+                }
+            }
+        }
+    }
+    
+    private static void populate(String contents){
         contents = contents.replaceAll("\"", ",");
         String[] names = contents.split(",");
         for(int i = 0; i < names.length; i++){
@@ -50,82 +138,6 @@ public class LastChance {
             everyone.put(person.getName(),person);
             while(!names[i].equals("new person")){
                 i++;
-            }
-        }
-        generateMatches();
-        if(enter.equals("sendForm" )){
-            System.out.println("Emailing the entire class of 2014, are you sure? y/n");
-            if(scanner.nextLine().equals("y")){
-                System.out.println("Please enter the name of a file containing email addresses.");
-                String fileName = scanner.nextLine();
-                String emailList = new Scanner(new File(fileName)).useDelimiter("\\Z").next();
-                String[] emails = emailList.split("\n");
-                SendEmail.sendForm(emails);
-            }
-        } else if(enter.equals("sendMatches")){
-            System.out.println("Emailing the entire class of 2014, are you sure? y/n");
-            if(scanner.nextLine().equals("y")){
-                SendEmail.sendMatches(everyone);
-            }
-        } else if(enter.equals("exit")){
-            System.out.println("Goodbye");
-        } 
-        else {
-            System.out.println("Specify \"matches\" to see someone's matches, \"choices\" to see someone's"
-                    + " choices, or \"me\" to see who listed your name.");
-            String mode = scanner.nextLine();
-            if(mode.equals("exit")){
-                mode = "exit";
-            }
-            else{
-                while(!mode.equals("matches") && !mode.equals("choices") && !mode.equals("me")){
-                    System.out.println();
-                    System.out.println("You must specify \"matches\", \"choices\", or \"me\"");
-                    mode = scanner.nextLine();
-                }
-            }
-            String s = mode;
-            while(!s.equals("exit")){
-                try{
-                    if(s.equals("matches") || s.equals("choices") || s.equals("me")){
-                        mode = s;
-                        System.out.println("Name?");
-                        s = scanner.nextLine();
-                    }
-                    if(mode.equals("matches")){
-                        everyone.get(s).printMatches();
-                        System.out.println();
-                    } else if(mode.equals("choices")){
-                        everyone.get(s).printChoices();
-                        System.out.println();
-                    } if(mode.equals("me")){
-                        for(Person p : everyone.values()){
-                            if(p.getChoices().contains(s)){
-                                System.out.println(p.getName());
-                            }
-                        }
-                        System.out.println();
-                    }
-                    s = scanner.nextLine();
-                }catch (NullPointerException e){
-                    System.out.println("Name not in system.");
-                    s = scanner.nextLine();
-                }
-            }
-            System.out.println("Goodbye");
-        }
-    }
-    
-    private static void generateMatches(){
-        for(Person person : everyone.values()){
-            ArrayList<String> choices = person.getChoices();
-            for(String s : choices){
-                if(everyone.containsKey(s)){
-                    Person possibleMatch = everyone.get(s);
-                        if(possibleMatch.getChoices().contains(person.getName())){
-                            person.addMatch(s);
-                        }
-                }
             }
         }
     }
